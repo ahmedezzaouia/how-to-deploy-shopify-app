eb3c7618819d0e80ccab4d3399aa72ea5edc1526
ahmedezzaouia <35857662+ahmedezzaouia@users.noreply.github.com>
2022-02-28 00:51:48 +0100

    add host

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ export default function Index() {
   return (
     <Page>
       <Heading>Shopify app with Node and React</Heading>
+      <div>
+        <button>Click Me</button>
+      </div>
     </Page>
   );
 }
